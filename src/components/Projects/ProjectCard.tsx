b7c78c39e3dc01de1f06460872abdb6b0b709d6a
Aron Matoic <128235732+ParalelSt@users.noreply.github.com>
2026-01-14 19:33:12 +0100

    feat: added github repo link button, show more button for project desc

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -1,4 +1,6 @@
 import type { Project } from "@/pages/Projects";
+import { useRef, useState } from "react";
+import { FaGithub } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 interface ProjectCardProp {
@@ -6,22 +8,58 @@ interface ProjectCardProp {
 }
 
 const ProjectCard = ({ project }: ProjectCardProp) => {
+  const descriptionRef = useRef<HTMLParagraphElement>(null);
+
+  const [buttonTextToggle, setButtonTextToggle] = useState<
+    "Show More" | "Show Less"
+  >("Show More");
+
+  const expandDesc = () => {
+    if (buttonTextToggle === "Show More") {
+      descriptionRef?.current?.classList.add("h-auto");
+      descriptionRef?.current?.classList.remove("h-5");
+
+      setButtonTextToggle("Show Less");
+    } else {
+      descriptionRef?.current?.classList.add("h-5");
+      descriptionRef?.current?.classList.remove("h-auto");
+      setButtonTextToggle("Show More");
+    }
+  };
+
   return (
-    <Link
-      to={project.link}
-      className="flex flex-col w-full h-full border-2 border-accent rounded-lg"
-    >
-      <div className="flex w-full min-h-40 justify-center items-center rounded-tl-lg rounded-tr-lg overflow-hidden">
+    <div className="flex flex-col w-full h-full border-2 border-accent rounded-lg lg:max-w-150">
+      <Link
+        to={project.link}
+        className="flex w-full min-h-40 justify-center items-center rounded-tl-lg rounded-tr-lg overflow-hidden"
+      >
         <img
           className="w-auto h-auto object-cover rounded-tl-lg rounded-tr-lg"
           src={project.image}
           alt="project-image"
         />
-      </div>
+      </Link>
       <div className="flex flex-col justify-center px-5 pt-3 pb-4 rounded-bl-lg rounded-br-lg">
-        <h3>{project.name}</h3>
-        <p className="mt-3 text-sm">{project.description}</p>
-        <div className="flex flex-wrap gap-2 mt-3">
+        <h3 className="font-bold">{project.name}</h3>
+        <p className="h-5 mt-3 text-sm overflow-y-hidden" ref={descriptionRef}>
+          {project.description}
+        </p>
+        <div className="flex gap-2 items-center">
+          <Link
+            to={project.repoLink}
+            className="flex justify-center items-center gap-1 w-25 mt-2 p-1.5 text-sm border-1 border-transparent rounded-xl bg-accent-hover btn-highlight"
+          >
+            <FaGithub className="bg-transparent" />
+            <span className="font-semibold bg-transparent">GitHub</span>
+          </Link>
+          <button
+            className="flex justify-center items-center w-25 mt-2 p-1.5 text-sm border-1 rounded-xl cursor-pointer btn-highlight"
+            onClick={expandDesc}
+          >
+            {buttonTextToggle}
+          </button>
+        </div>
+        <div className="flex flex-wrap items-start gap-2 mt-3">
           {project.stack.map((t) => (
             <span
               className="p-2 border-1 border-border rounded-xl text-sm"
@@ -32,7 +70,7 @@ const ProjectCard = ({ project }: ProjectCardProp) => {
           ))}
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
 
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -7,6 +7,7 @@ export interface Project {
   image: string;
   link: string;
   stack: string[];
+  repoLink: string;
 }
 
 const Projects = () => {
@@ -16,7 +17,7 @@ const Projects = () => {
       name: "ByteForge Webshop",
       description:
         "ByteForge is a full-stack gaming and tech webshop offering (fake) products such as GFUEL, gaming peripherals, PC components, accessories, and games. It is built with React and TypeScript on the frontend, styled using SCSS, and powered by a Node.js + Express backend with Supabase for database and authentication. The project is deployed on Vercel.",
-      image: "../public/images/ByteForge.png",
+      image: "../images/ByteForge.png",
       link: "https://byte-forge-rho.vercel.app/",
       stack: [
         "React",
@@ -27,23 +28,27 @@ const Projects = () => {
         "Supabase",
         "Vercel",
       ],
+      repoLink: "https://github.com/ParalelSt/ByteForge",
     },
 
     {
       id: 2,
       name: "Aron's Handbook (Gym Logbook)",
-      description: "",
-      image: "../public/images/AronSHandBook.png",
+      description:
+        "Aron's Handbook is a gym logbook application for tracking workouts, including reps and weights, while supporting training templates, structured programs, and personal goals. It is built with React and TypeScript, styled using Tailwind CSS, powered by Supabase for backend and authentication, deployed on Vercel, and developed with the help of AI-assisted prompts.",
+      image: "../images/AronSHandBook.png",
       link: "https://arons-handbook.vercel.app/login",
       stack: [
         "React",
         "Typescript",
-        "SCSS",
+        "Tailwind",
         "Node.js",
         "Express.js",
         "Supabase",
         "Vercel",
+        "AI development",
       ],
+      repoLink: "https://github.com/ParalelSt/Arons-handbook",
     },
   ];
 
@@ -51,7 +56,7 @@ const Projects = () => {
     <div className="flex flex-col w-full min-h-full mb-auto p-1 pt-5">
       <h2>Projects</h2>
 
-      <div className="flex flex-col md:flex-row gap-4 py-5 w-full justify-items-center">
+      <div className="flex flex-col md:flex-row gap-4 py-7 w-full justify-between items-stretch">
         {projects.map((p: Project) => (
           <ProjectCard project={p} key={p.id} />
         ))}
