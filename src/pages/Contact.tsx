07d42cdff1590a707deca52c8b056356740da7f6
Aron Matoic <128235732+ParalelSt@users.noreply.github.com>
2026-01-21 17:32:01 +0100

    fix: Contact page layout

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -15,7 +15,7 @@ const Contact = () => {
           <ContactForm className="mt-3" />
         </div>
         <div className="flex mt-2 lg:mt-0 items-start">
-          <ContactInfo className="w-full md:w-sm lg:max-w-lg" />
+          <ContactInfo className="w-full md:w-sm lg:w-115" />
         </div>
       </div>
     </div>
